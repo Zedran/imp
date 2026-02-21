2b86bfc78ff6a0388447971d34711ec3267bdfa3
Zedran <17592740+Zedran@users.noreply.github.com>
2026-02-21 12:45:53 +0100

    License the project under GPLv3

--- internal/utils/Preset.go
+++ internal/utils/Preset.go
@@ -1,3 +1,20 @@
+// imp -- tool for rewriting CSV files and normalizing encoding.
+// Copyright (C) 2026  Wojciech Głąb (github.com/Zedran)
+//
+// This file is part of imp.
+//
+// imp is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3 only.
+//
+// imp is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with imp.  If not, see <https://www.gnu.org/licenses/>.
+
 package utils
 
 import (
